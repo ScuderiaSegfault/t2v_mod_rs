e296dd16793e0cd6a8caedd36377900a4268fdcd
Felix Resch <felix.resch@tuwien.ac.at>
2026-05-06 13:48:24 +0200

    reworked implementation of T2V handling in code for better readability

--- t2v_driver/src/unit.rs
+++ t2v_driver/src/unit.rs
@@ -2,14 +2,13 @@ use crate::StatusNotifier;
 use async_std::os::unix::net::UnixDatagram;
 use async_std::stream::{Stream, StreamExt};
 use futures::channel::mpsc::{Sender, channel};
-use futures::future::Either;
 use futures::{SinkExt, pin_mut};
 use log::{debug, error, info, warn};
 use std::collections::HashSet;
 use std::os::unix::net::SocketAddr;
-use t2v_driver_proto::{Event, Request};
 use t2v_module::{Initial, IrNecFrame, T2VModule};
-use tokio::task;
+use t2v_driver_proto::{Event as T2VEvent, Request};
+use tokio::{select, task};
 
 pub async fn unit<S: Stream<Item = T2VModule<Initial>> + Unpin>(
     mut devices: S,
@@ -20,27 +19,31 @@ pub async fn unit<S: Stream<Item = T2VModule<Initial>> + Unpin>(
     notifier.ready();
     notifier.report_status("Ready".into());
 
-    let (sender, mut receiver) = channel(16);
+    let (ir_sender, mut ir_receiver) = channel(16);
 
-    let ir_nec_task = task::spawn(handle_device(device, sender.clone()));
+    let ir_nec_task = task::spawn(handle_device(
+        device,
+        ir_sender.clone(),
+    ));
     pin_mut!(ir_nec_task);
 
     let mut ir_nec_subscribers = HashSet::new();
 
     loop {
-        let handle_incoming = handle_incoming(socket);
-        pin_mut!(handle_incoming);
+        enum Event {
+            IncomingPacket(crate::Result<Option<(Request, SocketAddr)>>),
+            IrFrame(Option<IrNecFrame>),
+            TaskExit(crate::Result<()>),
+        }
 
-        let receive_frame = receiver.next();
-        pin_mut!(receive_frame);
+        let event = select! {
+            incoming = handle_incoming(socket) => Event::IncomingPacket(incoming),
+            ir_frame = ir_receiver.next() => Event::IrFrame(ir_frame),
+            task_exit = &mut ir_nec_task => Event::TaskExit(task_exit?),
+        };
 
-        match futures::future::select(
-            futures::future::select(handle_incoming, receive_frame),
-            &mut ir_nec_task,
-        )
-        .await
-        {
-            Either::Left((Either::Left((request, _)), _)) => {
+        match event {
+            Event::IncomingPacket(request) => {
                 debug!("incoming request: {:?}", request);
                 match request {
                     Ok(Some((request, addr))) => match request {
@@ -58,9 +61,9 @@ pub async fn unit<S: Stream<Item = T2VModule<Initial>> + Unpin>(
                     }
                 }
             }
-            Either::Left((Either::Right((frame, _)), _)) => {
+            Event::IrFrame(frame) => {
                 if !ir_nec_subscribers.is_empty() {
-                    let data = serde_json::to_vec(&Event::IrNecFrame(frame.unwrap())).unwrap();
+                    let data = serde_json::to_vec(&T2VEvent::IrNecFrame(frame.unwrap())).unwrap();
                     for ir_nec_subscriber in &ir_nec_subscribers {
                         debug!("sending event to: {}", ir_nec_subscriber.display());
                         if let Err(e) = socket.send_to(&data, ir_nec_subscriber).await {
@@ -72,10 +75,10 @@ pub async fn unit<S: Stream<Item = T2VModule<Initial>> + Unpin>(
                     }
                 }
             }
-            Either::Right((_, _)) => {
+            Event::TaskExit(_result) => {
                 warn!("device closed, trying next one");
                 notifier.report_status("Reconnecting".into());
-                let data = serde_json::to_vec(&Event::Disconnected).unwrap();
+                let data = serde_json::to_vec(&T2VEvent::Disconnected).unwrap();
                 for ir_nec_subscriber in &ir_nec_subscribers {
                     debug!("sending event to: {}", ir_nec_subscriber.display());
                     if let Err(e) = socket.send_to(&data, ir_nec_subscriber).await {
@@ -87,7 +90,7 @@ pub async fn unit<S: Stream<Item = T2VModule<Initial>> + Unpin>(
                 }
                 let device = devices.next().await.expect("no device found");
                 notifier.report_status("Connected".into());
-                let data = serde_json::to_vec(&Event::Connected).unwrap();
+                let data = serde_json::to_vec(&T2VEvent::Connected).unwrap();
                 for ir_nec_subscriber in &ir_nec_subscribers {
                     debug!("sending event to: {}", ir_nec_subscriber.display());
                     if let Err(e) = socket.send_to(&data, ir_nec_subscriber).await {
@@ -97,7 +100,10 @@ pub async fn unit<S: Stream<Item = T2VModule<Initial>> + Unpin>(
                         );
                     };
                 }
-                *ir_nec_task = task::spawn(handle_device(device, sender.clone()));
+                *ir_nec_task = task::spawn(handle_device(
+                    device,
+                    ir_sender.clone(),
+                ));
             }
         }
     }
@@ -119,7 +125,7 @@ async fn handle_incoming(socket: &UnixDatagram) -> crate::Result<Option<(Request
 
 pub async fn handle_device(
     device: T2VModule<Initial>,
-    mut sender: Sender<IrNecFrame>,
+    mut ir_sender: Sender<IrNecFrame>,
 ) -> crate::Result<()> {
     match (device.manufacturer_string(), device.product_string()) {
         (Some(manufacturer_string), Some(product_string)) => {
@@ -162,21 +168,28 @@ pub async fn handle_device(
     let mut ir_nec_reader = device.ir_nec_endpoint()?;
 
     loop {
-        match ir_nec_reader.next().await {
-            Ok(option) => match option {
+        enum Event {
+            Ir(Result<Option<IrNecFrame>, tokio::io::Error>),
+        }
+        let event = select! {
+            ir_frame = ir_nec_reader.next() => Event::Ir(ir_frame),
+        };
+
+        match event {
+            Event::Ir(Ok(option)) => match option {
                 None => {
-                    debug!("End of received messages");
+                    debug!("End of IR frames");
                     return Ok(());
                 }
                 Some(frame) => {
                     info!("Received frame {frame:02x?}");
-                    if let Err(e) = sender.send(frame).await {
+                    if let Err(e) = ir_sender.send(frame).await {
                         error!("failed to send frame: {e}");
                     }
                 }
             },
-            Err(e) => {
-                error!("Error while receiving messages {e:?}");
+            Event::Ir(Err(e)) => {
+                error!("Error while receiving IR frames: {e:?}");
                 return Err(e.into());
             }
         }
